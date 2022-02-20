864a83a7f08d10d4ac6e790b8214323f8bd4255a
John Hook <jhook1@ufl.edu>
2022-02-19 20:29:03 -0500

    Completed parts 4 and 5. Added search/filter components

--- netwebapp/Models/Movie.cs
+++ netwebapp/Models/Movie.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace netwebapp.Models
 {
@@ -7,10 +8,13 @@ namespace netwebapp.Models
         public int ID { get; set; }
         public string Title { get; set; } = string.Empty;
 
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)] //sets DateTime type to display as Date
         public DateTime ReleaseDate { get; set; }
         
         public string Genre { get; set; } = string.Empty;
+
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
     }
 }
--- netwebapp/Pages/Movies/Index.cshtml.cs
+++ netwebapp/Pages/Movies/Index.cshtml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using netwebapp.Data;
 using netwebapp.Models;
@@ -22,9 +23,36 @@ namespace netwebapp.Pages.Movies
 
         public IList<Movie> Movie { get;set; }
 
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+
+        public SelectList Genres { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string MovieGenre { get; set; }
+
         public async Task OnGetAsync()
         {
-            Movie = await _context.Movie.ToListAsync();
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+
+            var movies = from m in _context.Movie
+                         select m;
+
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                //Where modifies query attached to movies
+                movies = movies.Where(s => s.Title.Contains(SearchString));
+            }
+            if (!string.IsNullOrEmpty(MovieGenre))
+            {
+                movies = movies.Where(x => x.Genre == MovieGenre);
+            }
+
+            //execution of LINQ queries is deferred until this point
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+            Movie = await movies.ToListAsync();
         }
     }
 }
